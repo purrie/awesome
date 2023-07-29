2ee8d6f7785206871c08cfad6067b812b76f668a
Purrie <purriebrightstar@gmail.com>
2023-07-29 11:26:46 +0200

    formatting

--- window-rules.lua
+++ window-rules.lua
@@ -5,75 +5,74 @@ local client_bindings = require("client-bindings")
 -- Rules to apply to new clients (through the "manage" signal).
 awful.rules.rules = {
     -- All clients will match this rule.
-    { rule = { },
-      properties = { border_width = beautiful.border_width,
-                     border_color = beautiful.border_normal,
-                     focus = awful.client.focus.filter,
-                     raise = true,
-                     keys = client_bindings.clientkeys,
-                     buttons = client_bindings.clientbuttons,
-                     screen = awful.screen.preferred,
-                     placement = awful.placement.no_overlap+awful.placement.no_offscreen
-     }
+    {
+        rule = { },
+        properties = {
+            border_width = beautiful.border_width,
+            border_color = beautiful.border_normal,
+            focus = awful.client.focus.filter,
+            raise = true,
+            keys = client_bindings.clientkeys,
+            buttons = client_bindings.clientbuttons,
+            screen = awful.screen.preferred,
+            placement = awful.placement.no_overlap + awful.placement.no_offscreen
+        }
     },
 
     -- Floating clients.
-    { rule_any = {
-        instance = {
-          "DTA",  -- Firefox addon DownThemAll.
-          "copyq",  -- Includes session name in class.
-          "pinentry",
-        },
-        class = {
-          "Arandr",
-          "Blueman-manager",
-          "Gpick",
-          "Kruler",
-          "MessageWin",  -- kalarm.
-          "Sxiv",
-          "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
-          "Wpa_gui",
-          "veromix",
-          "xtightvncviewer"},
+    {
+        rule_any = {
+            instance = {
+                "DTA",  -- Firefox addon DownThemAll.
+                "copyq",  -- Includes session name in class.
+                "pinentry",
+            },
+            class = {
+                "Arandr",
+                "Blueman-manager",
+                "Gpick",
+                "Kruler",
+                "MessageWin",  -- kalarm.
+                "Sxiv",
+                "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
+                "Wpa_gui",
+                "veromix",
+                "xtightvncviewer"
+            },
 
-        -- Note that the name property shown in xprop might be set slightly after creation of the client
-        -- and the name shown there might not match defined rules here.
-        name = {
-          "Event Tester",  -- xev.
+            -- Note that the name property shown in xprop might be set slightly after creation of the client
+            -- and the name shown there might not match defined rules here.
+            name = {
+                "Event Tester",  -- xev.
+            },
+            role = {
+                "AlarmWindow",  -- Thunderbird's calendar.
+                "ConfigManager",  -- Thunderbird's about:config.
+                "pop-up",       -- e.g. Google Chrome's (detached) Developer Tools.
+            }
         },
-        role = {
-          "AlarmWindow",  -- Thunderbird's calendar.
-          "ConfigManager",  -- Thunderbird's about:config.
-          "pop-up",       -- e.g. Google Chrome's (detached) Developer Tools.
-        }
-      }, properties = { floating = true }},
+        properties = { floating = true }
+    },
 
     -- Add titlebars to normal clients and dialogs
     {
-      rule_any = {
-        type = { "normal" }
-      },
-      properties = { titlebars_enabled = false }
-    },
-    {
-      rule_any = {
-        type = { "dialog" }
-      },
-      properties = {
-        titlebars_enabled = true,
-        floating = true,
-        ontop = true }
-    },
-
-    -- other programs
-    {
         rule_any = {
-            name = {
-                "Token Maker"
-            }
+            type = { "normal" }
         },
         properties = {
-            screen = 2,
+            titlebars_enabled = false
+        }
+    },
+
+    {
+        rule_any = {
+            type = { "dialog" }
+        },
+        properties = {
+            placement = awful.placement.under_mouse + awful.placement.no_offscreen,
+            titlebars_enabled = true,
+            floating = true,
+            ontop = true
         }
     },
 
